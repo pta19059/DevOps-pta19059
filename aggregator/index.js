2678084b2e08a7c110cc529ffbf11d078fad9363
Stefano Sguotti <58917457+pta19059@users.noreply.github.com>
2019-12-15 21:03:39 +0000

    Add files via upload

---
+++ aggregator/index.js
@@ -0,0 +1,27 @@
+const Hapi = require('hapi')
+const server = new Hapi.Server()
+let request = require('request-promise')
+
+server.connection({port: 8085})
+
+server.route({
+  method: 'GET',
+  path: '/dates/{timestamp}',
+  handler: async (req, reply) => {
+    const utcEndpoint = `http://utcdate-service/${req.params.timestamp}/utcdate`
+    const isoEndpoint = `http://isodate-service/${req.params.timestamp}/isodate`
+    let utcBody = await request(utcEndpoint)
+    let isoBody = await request(isoEndpoint)
+    reply({
+      utcDate: JSON.parse(utcBody).date,
+      isoDate: JSON.parse(isoBody).date
+    })
+  }
+})
+
+server.start((err) => {
+  if (err) {
+    throw err
+  }
+  console.log('aggregator started on port 8085')
+})
